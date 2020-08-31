1eb762dd94de351401a053acbf5ae95e48005416
Shelby Poston <shelbyp@gva-twn.com>
2020-08-31 16:32:35 -0400

    Add Banner and Search components daterange

--- src/Banner.js
+++ src/Banner.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Banner.css";
 import { Button } from "@material-ui/core";
+import Search from './Search';
 
 function Banner() {
+ const [showSearch, setShowShow] = useState(false);
+
   return (
     <div className="banner">
+      <div className="banner__search">
+        {showSearch && <h1> <Search /></h1>}
+        <Button onClick={()=> setShowShow(!showSearch)} className="banner__searchButton"
+        variant="outlined">Search Dates</Button>
+      </div>
       <div className="banner__info">
         <h1>Get out and stretch your imagination</h1>
         <h5>Plan a different kind of getaway to uncover the gems near you.</h5>
---
+++ src/Search.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import './Search.css';
+import { DateRange } from 'react-date-range';
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+
+function Search() {
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+
+    cons selectionRange = {
+        startDate = startDate,
+        endDate = endDate,
+        key: "selection",
+    };
+
+
+      function handleSelect(range){
+          setStartDate(range.selection.startDate);
+          setEndDate(range.selection.endDate);
+      }
+    return (
+        <div className="search">
+            <h1> SHOW DATE PICKER</h1>
+        </div>
+    )
+}
+
+export default Search
